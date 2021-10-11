1276b3357be9c9d3836fffdedf62e2ac3f7c5c64
lcg0826 <changyu0826@naver.com>
2021-10-11 19:55:32 +0900

    BOJ 1032 '명령 프롬프트'

---
+++ CodingTest_Practice_Java/src/BOJ_1032.java
@@ -0,0 +1,39 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ_1032 {
+
+	public static void main(String[] args) throws IOException {
+	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	    int N = Integer.parseInt(br.readLine());
+	    
+	    StringBuilder pattern = new StringBuilder();
+	    boolean sameCheck = true;
+
+	    String[] arr = new String[N];
+	    for(int i = 0; i < N; i++) {
+	        arr[i] = br.readLine();
+	    }
+
+	    for(int i=0; i<arr[0].length(); i++) {
+	        for(int j=1; j<N; j++) {
+	            if(arr[0].charAt(i) == arr[j].charAt(i)) {
+	            	sameCheck = true;
+	            }else {
+	            	sameCheck = false;
+	                break;
+	            }
+	        }
+
+	        if(!sameCheck) {
+	            pattern.append("?");
+	        }else {
+	            pattern.append(arr[0].charAt(i));
+	        }
+	    }
+	    System.out.println(pattern.toString());
+	}
+}
+
