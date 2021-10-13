3aa0a8a8b6f9beb4c86d370996de0b90ccae9597
lcg0826 <changyu0826@naver.com>
2021-10-13 23:54:43 +0900

    BOJ 2839 '설탕배달'

---
+++ CodingTest_Practice_Java/src/bronze_I/BOJ_2839.java
@@ -0,0 +1,35 @@
+package bronze_I;
+
+import java.util.Scanner;
+
+public class BOJ_2839 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		
+		int hap = 0;
+		
+		while(true) {
+			if(n % 5 == 0) {
+				hap += n / 5;
+				System.out.println(hap);
+				break;
+			} else {
+				n -= 3;
+				hap++;
+			}
+			
+			if(n < 0) {
+				System.out.println("-1");
+				break;
+			}
+			
+		}
+			
+	
+	}
+
+}
