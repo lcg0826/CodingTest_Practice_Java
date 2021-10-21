3717df494fd343b9f73441b99ebeb02c27d920ba
lcg0826 <changyu0826@naver.com>
2021-10-21 22:32:33 +0900

    시간 초과 -> 다시 도전

---
+++ CodingTest_Practice_Java/src/silver_V/BOJ_11004.java
@@ -0,0 +1,28 @@
+package silver_V;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class BOJ_11004 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		int m = sc.nextInt();
+		
+		int[] sortArr = new int[n];
+		
+		for ( int i = 0; i < n; i++) {
+			sortArr[i] = sc.nextInt();
+		}
+		
+		Arrays.sort(sortArr);
+		
+		System.out.println(sortArr[m-1]);
+		
+	}
+
+}
